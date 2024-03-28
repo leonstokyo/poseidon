b02829c96cb7967fd2aa267113dbd6931b1f93fc
longtao.guan <longtao.guan@okcoin.jp>
2024-03-28 15:55:28 +0900

    init module

---
+++ poseidon-rest/src/main/java/jp/tokyo/leon/rest/PoseidonRestApplication.java
@@ -0,0 +1,16 @@
+package jp.tokyo.leon.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author longtao.guan
+ */
+@SpringBootApplication
+public class PoseidonRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoseidonRestApplication.class);
+    }
+
+}
